1f35805a6684a6b05c83f21aa9a3d74f0adfae4f
FsqlKEYoTJ <nao.enpg@gmail.com>
2024-05-03 13:34:44 +0900

    クイズ機能実装

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,12 +11,15 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   private val quiz = Quiz(
     List(
       Question(1, "今日食べたものが、マック、焼肉、ラーメン、アイスって完全に脂肪フラグ", List("校長", "ひろゆき"), 0),
-      Question(2, "「飲み会のシメはラーメンじゃなくて、ステーキだよ」という思想の人達と飲みました", List("校長", "ひろゆき"), 0),
-      Question(3, "痩せたくないなら痩せなくていいんじゃないですか？", List("校長", "ひろゆき"), 0),
-      Question(4, "昼はコーヒーミーティング。夜はラーメンミーティング。", List("校長", "ひろゆき"), 0),
-      Question(5, "頭の中ではメッシやクリスティアーノ・ロナウドを超えるプレーをしている", List("校長", "ひろゆき", "本田圭佑"), 0),
-      Question(6, "頭悪いんだから独学止めた方がいいっすよ", List("校長", "ひろゆき"), 0),
+      Question(2, "「飲み会のシメはラーメンじゃなくて、ステーキだよ」という思想の人達と飲みました", List("校長", "ひろゆき"), 1),
+      Question(3, "痩せたくないなら痩せなくていいんじゃないですか？", List("校長", "ひろゆき"), 1),
+      Question(4, "酒を飲んだらSNSでやることはただ1つです。それは「ラーメンをアップする」です", List("校長", "ひろゆき"), 0),
+      Question(5, "頭の中ではメッシやクリスティアーノ・ロナウドを超えるプレーをしている", List("校長", "ひろゆき", "本田圭佑"), 2),
+      Question(6, "頭悪いんだから独学止めた方がいいっすよ", List("校長", "ひろゆき"), 1),
       Question(7, "あなたが避けられてるんじゃないですかね？", List("校長", "ひろゆき"), 0),
+      Question(8, "95%のエンジニアはいらないんじゃないかって思っちゃうんですけど", List("校長", "ひろゆき", "せとゆき"), 2),
+      Question(9, "プログラムって1個できるようになると他の言語に行くの簡単なんですよ", List("校長", "ひろゆき"), 1),
+      Question(10, "Vimはほとんどやらないです、はい、すみません。", List("校長", "ひろゆき"), 1),
     )
   )
 
@@ -25,7 +28,17 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   }
 
   def submit = Action { implicit request: Request[AnyContent] =>
-    // フォームから回答を取得し、正誤を評価するロジックをここに記述
-    Ok("結果")
+  val answers = request.body.asFormUrlEncoded.getOrElse(Map.empty).flatMap { case (key, values) =>
+    key.toIntOption.flatMap { intKey =>
+      values.headOption.flatMap(_.toIntOption).map(intKey -> _)
+    }
+  }
+
+  val results = quiz.questions.map { question =>
+    val userAnswer = answers.getOrElse(question.id, -1)
+    question -> (userAnswer == question.correctAnswer)
+  }
+
+  Ok(views.html.result(results)) // 結果表示画面に結果を渡す
   }
 }
